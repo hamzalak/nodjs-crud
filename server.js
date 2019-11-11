150409ab4cbd409bbe29bae4d56f9c905a2990ce
hamzalak <lakhal1hamza@gmail.com>
2019-11-11 05:45:07 +0100

    remarque : on pourra utiliser une api externe si elle fournit les produits au lieu de celle qui est interne

--- server.js
+++ server.js
@@ -5,10 +5,44 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 // create an instance of express to serve our end points
 const app = express();
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
+const port = 3001 ; 
 
 app.use(cors({
   origin: 'http://localhost:3000' 
 }));
+/*********************************socket io logic****************************/
+
+const server = http.createServer(app);
+    /********************************************************/
+    const io = socketIo(server); 
+    const getApiAndEmit = async socket => {
+
+      try {
+        const res = await  axios.get(
+          `http://localhost:3001/products`
+        ); 
+        socket.emit("FromAPI", res.data );  
+      } catch (error) {
+        console.error(`Error: ${error.code}`);
+      }
+    };
+    /*******************************************/
+    let interval;
+    io.on("connection", socket => {
+        console.log("New client connected");
+        if (interval) {
+        clearInterval(interval);
+        }
+        interval = setInterval(() => getApiAndEmit(socket), 1000);
+        socket.on("disconnect", () => {
+        console.log("Client disconnected");
+        });
+     }) ; 
+
+/***********************************************************************/
 // we'll load up node's built in file system helper library here
 // (we'll be using this later to serve our JSON files
 const fs = require('fs');
@@ -22,6 +56,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const routes = require('./routes/routes.js')(app, fs);
 
 // finally, launch our server on port 3001.
-const server = app.listen(3001, () => {
-    console.log('listening on port %s...', server.address().port);
-});
+server.listen(port, () => console.log(`Listening on port ${port}`));
+
+ 
+  
+
+
